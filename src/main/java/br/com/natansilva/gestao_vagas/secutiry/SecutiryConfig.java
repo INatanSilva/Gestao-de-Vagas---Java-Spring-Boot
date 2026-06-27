d1a79f8b820577a962386fbdfeb7ca8a5f81d36f
Natan Silva <natansilva@MacBook-Air.Home>
2026-06-27 16:41:32 +0100

    Implementa filtro de segurança e expiração de token JWT
    Adiciona SecurityFilter como componente Spring, conecta-o ao fluxo de segurança antes do BasicAuthenticationFilter, e define validade de 2 horas para tokens JWT. Remove importação redundante em AuthCompanyController.

--- src/main/java/br/com/natansilva/gestao_vagas/secutiry/SecutiryConfig.java
+++ src/main/java/br/com/natansilva/gestao_vagas/secutiry/SecutiryConfig.java
@@ -1,15 +1,20 @@
 package br.com.natansilva.gestao_vagas.secutiry;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 public class SecutiryConfig {
     
+    @Autowired
+    private SecurityFilter securityFilter;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
@@ -19,8 +24,11 @@ public class SecutiryConfig {
                 .requestMatchers("/companies/").permitAll()
                 .requestMatchers("/auth/companies").permitAll()
                 .anyRequest().authenticated()
-            );
+            )
+            .addFilterBefore(securityFilter, BasicAuthenticationFilter.class);
 
+            ;
+            
         return http.build();
     }
 
